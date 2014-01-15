305dabf21b0920b2c968ca1c9e3332b8c33908fe
Matt Todd <matt.c.todd@gmail.com>
2014-01-15 16:04:07 -0600

    adding gatling

---
+++ gatling-charts-highcharts-1.5.3/results/basicexamplesimulation-20140115154035/js/stats.js
@@ -0,0 +1,817 @@
+var stats = {
+	type: "GROUP",
+contents: {
+		
+				"request-1-46da482b5ba7614b7124accf72d8b1ce": {
+		type: "REQUEST",
+		name: "request_1",
+path: "request_1",
+pathFormatted: "request-1-46da482b5ba7614b7124accf72d8b1ce",
+stats: {
+	numberOfRequests : {
+		total: "10",
+		ok: "10",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "70",
+		ok: "70",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "150",
+		ok: "150",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "90",
+		ok: "90",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "21",
+		ok: "21",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "150",
+		ok: "150",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "150",
+		ok: "150",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 10,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-2-93baff648d9a0c13a48ee1d38e7b220f": {
+		type: "REQUEST",
+		name: "request_2",
+path: "request_2",
+pathFormatted: "request-2-93baff648d9a0c13a48ee1d38e7b220f",
+stats: {
+	numberOfRequests : {
+		total: "10",
+		ok: "10",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "50",
+		ok: "50",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "43",
+		ok: "43",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "5",
+		ok: "5",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "50",
+		ok: "50",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "50",
+		ok: "50",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 10,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-3-d09735bb02d6e5012b92b4f819ff64cc": {
+		type: "REQUEST",
+		name: "request_3",
+path: "request_3",
+pathFormatted: "request-3-d09735bb02d6e5012b92b4f819ff64cc",
+stats: {
+	numberOfRequests : {
+		total: "10",
+		ok: "10",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "70",
+		ok: "70",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "47",
+		ok: "47",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "9",
+		ok: "9",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "70",
+		ok: "70",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "70",
+		ok: "70",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 10,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-4-e7d1b1bd5d422b9c4051b94ac981cc91": {
+		type: "REQUEST",
+		name: "request_4",
+path: "request_4",
+pathFormatted: "request-4-e7d1b1bd5d422b9c4051b94ac981cc91",
+stats: {
+	numberOfRequests : {
+		total: "50",
+		ok: "50",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "50",
+		ok: "50",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "1000",
+		ok: "1000",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "112",
+		ok: "112",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "178",
+		ok: "178",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "470",
+		ok: "470",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "1000",
+		ok: "1000",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 48,
+		percentage: 96
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 2,
+		percentage: 4
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-5-48829dc27f9f565bbe1021ea0e005040": {
+		type: "REQUEST",
+		name: "request_5",
+path: "request_5",
+pathFormatted: "request-5-48829dc27f9f565bbe1021ea0e005040",
+stats: {
+	numberOfRequests : {
+		total: "50",
+		ok: "0",
+		ko: "50"
+	},
+	minResponseTime : {
+		total: "70",
+		ok: "-",
+		ko: "70"
+	},
+	maxResponseTime : {
+		total: "240",
+		ok: "-",
+		ko: "240"
+	},
+	meanResponseTime : {
+		total: "116",
+		ok: "-",
+		ko: "116"
+	},
+	standardDeviation : {
+		total: "35",
+		ok: "-",
+		ko: "35"
+	},
+	percentiles1 : {
+		total: "190",
+		ok: "-",
+		ko: "190"
+	},
+	percentiles2 : {
+		total: "240",
+		ok: "-",
+		ko: "240"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 0,
+		percentage: 0
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 50,
+		percentage: 100
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "-",
+		ko: "0"
+	}
+}
+
+
+	}
+		,		
+				"request-6-027a986b276e85edb9fc05dfa564c868": {
+		type: "REQUEST",
+		name: "request_6",
+path: "request_6",
+pathFormatted: "request-6-027a986b276e85edb9fc05dfa564c868",
+stats: {
+	numberOfRequests : {
+		total: "50",
+		ok: "50",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "120",
+		ok: "120",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "65",
+		ok: "65",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "21",
+		ok: "21",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "120",
+		ok: "120",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "120",
+		ok: "120",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 50,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-7-f222f67b6eaeefa6fd794f5864b52d5a": {
+		type: "REQUEST",
+		name: "request_7",
+path: "request_7",
+pathFormatted: "request-7-f222f67b6eaeefa6fd794f5864b52d5a",
+stats: {
+	numberOfRequests : {
+		total: "50",
+		ok: "0",
+		ko: "50"
+	},
+	minResponseTime : {
+		total: "70",
+		ok: "-",
+		ko: "70"
+	},
+	maxResponseTime : {
+		total: "860",
+		ok: "-",
+		ko: "860"
+	},
+	meanResponseTime : {
+		total: "110",
+		ok: "-",
+		ko: "110"
+	},
+	standardDeviation : {
+		total: "109",
+		ok: "-",
+		ko: "109"
+	},
+	percentiles1 : {
+		total: "140",
+		ok: "-",
+		ko: "140"
+	},
+	percentiles2 : {
+		total: "860",
+		ok: "-",
+		ko: "860"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 0,
+		percentage: 0
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 50,
+		percentage: 100
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "-",
+		ko: "0"
+	}
+}
+
+
+	}
+		,		
+				"request-8-ef0c844bd0bb2bd86cddee05b47dc1ed": {
+		type: "REQUEST",
+		name: "request_8",
+path: "request_8",
+pathFormatted: "request-8-ef0c844bd0bb2bd86cddee05b47dc1ed",
+stats: {
+	numberOfRequests : {
+		total: "50",
+		ok: "50",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "210",
+		ok: "210",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "67",
+		ok: "67",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "29",
+		ok: "29",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "120",
+		ok: "120",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "210",
+		ok: "210",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 50,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-9-d127ea1ed17ba130bfaea83427418f34": {
+		type: "REQUEST",
+		name: "request_9",
+path: "request_9",
+pathFormatted: "request-9-d127ea1ed17ba130bfaea83427418f34",
+stats: {
+	numberOfRequests : {
+		total: "10",
+		ok: "10",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "440",
+		ok: "440",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "90",
+		ok: "90",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "119",
+		ok: "119",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "440",
+		ok: "440",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "440",
+		ok: "440",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 10,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		,		
+				"request-10-1cfbe0349999192f3f74942b295a69fe": {
+		type: "REQUEST",
+		name: "request_10",
+path: "request_10",
+pathFormatted: "request-10-1cfbe0349999192f3f74942b295a69fe",
+stats: {
+	numberOfRequests : {
+		total: "10",
+		ok: "10",
+		ko: "0"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	maxResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	meanResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	standardDeviation : {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	},
+	percentiles1 : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	percentiles2 : {
+		total: "40",
+		ok: "40",
+		ko: "-"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 10,
+		percentage: 100
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 0,
+		percentage: 0
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "0",
+		ok: "0",
+		ko: "-"
+	}
+}
+
+
+	}
+		},
+name: "Global Information",
+path: "",
+pathFormatted: "missing-name",
+stats: {
+	numberOfRequests : {
+		total: "300",
+		ok: "200",
+		ko: "100"
+	},
+	minResponseTime : {
+		total: "40",
+		ok: "40",
+		ko: "70"
+	},
+	maxResponseTime : {
+		total: "1000",
+		ok: "1000",
+		ko: "860"
+	},
+	meanResponseTime : {
+		total: "89",
+		ok: "76",
+		ko: "113"
+	},
+	standardDeviation : {
+		total: "94",
+		ok: "98",
+		ko: "81"
+	},
+	percentiles1 : {
+		total: "150",
+		ok: "120",
+		ko: "160"
+	},
+	percentiles2 : {
+		total: "470",
+		ok: "470",
+		ko: "240"
+	},
+	group1 : {
+		name: "t < 800 ms",
+		count: 198,
+		percentage: 66
+	},
+	group2 : {
+		name: "800 ms < t < 1200 ms",
+		count: 2,
+		percentage: 0
+	},
+	group3 : {
+		name: "t > 1200 ms",
+		count: 0,
+		percentage: 0
+	},
+	group4 : {
+		name: "failed",
+		count: 100,
+		percentage: 33
+	},
+	meanNumberOfRequestsPerSecond: {
+		total: "3",
+		ok: "2",
+		ko: "1"
+	}
+}
+
+
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
